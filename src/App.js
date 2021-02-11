67408121260c9c0027c006bf4510255a96da5952
Aksenov <ais924@yandex.ru>
2021-02-11 14:05:35 +0300

    Урок 11 Соц сеть. Наброски структуры

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
 import React from 'react';
 import './App.css';
-import Header from './Header';
-import Technologies from './Technologies';
-
 
 const App = () => {
   return (
-    <div className="App">
-      <Technologies/>
-      <Header/>
+    <div className="app-wrapper">
+      <header className='header'>
+        <img src='https://cdn.logo.com/hotlink-ok/logo-social-sq.png'/>
+      </header>
+
+      <nav className='nav'>
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+
+      <div className='content'>
+        <div>
+          <img src='https://zg66.ru/images/2020/07/15/black-sea_obl.jpg'/>
+        </div>
+        <div>
+          ava + description
+        </div>
+        <div>
+          My post
+        </div>
+        <div>
+          New post
+        </div>
+        <div>
+          post 1
+        </div>
+        <div>
+          post 2
+        </div>
+      </div>
+
     </div>
   );
 }
