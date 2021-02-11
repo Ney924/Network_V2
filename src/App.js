1d2f91d2e0a13611a66816e5a9cad64211b80040
Aksenov <ais924@yandex.ru>
2021-02-11 12:01:42 +0300

    Урок 07 Компонента

--- src/App.js
+++ src/App.js
@@ -4,14 +4,30 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      Simple HTML
-      <ul>
-        <li>css</li>
-        <li>html</li>
-        <li>js</li>
-      </ul>
+      
+      <Technologies/>
+      <Header/>
     </div>
   );
 }
+
+const Technologies = () => {
+  return(
+    <div>
+      dgfbvdfbdfbdfbdf
+    </div>
+  );
+}
+
+const Header = () => {
+  return (
+    <div>
+      <div>Home</div>
+      <div>News Feed</div>
+      <div>Messages</div>
+    </div>
+    
+  );
+} 
 
 export default App;
