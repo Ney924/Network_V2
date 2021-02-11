9cb6350fc0785f8cd702daf880239a1843a99658
Aksenov <ais924@yandex.ru>
2021-02-11 14:23:20 +0300

    Урок 12 Разбиваем соц сеть на базовые компоненты

--- src/App.js
+++ src/App.js
@@ -1,52 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import NavBar from './components/NavBar';
+import Profile from './components/Profile';
 
 const App = () => {
   return (
     <div className="app-wrapper">
-      <header className='header'>
-        <img src='https://cdn.logo.com/hotlink-ok/logo-social-sq.png'/>
-      </header>
-
-      <nav className='nav'>
-        <div>
-          <a>Profile</a>
-        </div>
-        <div>
-          <a>Messages</a>
-        </div>
-        <div>
-          <a>News</a>
-        </div>
-        <div>
-          <a>Music</a>
-        </div>
-        <div>
-          <a>Settings</a>
-        </div>
-      </nav>
-
-      <div className='content'>
-        <div>
-          <img src='https://zg66.ru/images/2020/07/15/black-sea_obl.jpg'/>
-        </div>
-        <div>
-          ava + description
-        </div>
-        <div>
-          My post
-        </div>
-        <div>
-          New post
-        </div>
-        <div>
-          post 1
-        </div>
-        <div>
-          post 2
-        </div>
-      </div>
-
+      <Header/>
+      <NavBar/>
+      <Profile/>
     </div>
   );
 }
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+
+const Profile = () => {
+        return (
+                <div className='content'>
+                <div>
+                  <img src='https://zg66.ru/images/2020/07/15/black-sea_obl.jpg'/>
+                </div>
+                <div>
+                  ava + description
+                </div>
+                <div>
+                  My post
+                </div>
+                <div>
+                  New post
+                </div>
+                <div>
+                  post 1
+                </div>
+                <div>
+                  post 2
+                </div>
+              </div>
+        );
+}
+
+
+export default Profile;
