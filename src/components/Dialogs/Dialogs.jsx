2a140df651f46b6ccebad87691445c36a65a2805
Aksenov <ais924@yandex.ru>
2021-02-12 11:42:21 +0300

    Урок 21 Верстаем страничку Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './Dialogs.css';
 
 const Dialogs = () => {
         return(
-                <div>
-                        Dialogs
+                <div className='dialogs'>
+                       <div className='dialog-items'>
+                                <NavLink to='Ilya'>Ilya</NavLink>  
+                                <NavLink to='Anna'>Anna</NavLink>
+                                <NavLink to='Aleksey'>Aleksey</NavLink>
+                                <NavLink to='Egor'>Egor</NavLink>       
+                       </div>
+                       
+                        <div className='messages'>
+                                <div className='message'>Привет</div>
+                                <div className='message'>Как дела</div>
+                                <div className='message'>Учу ссучий реакт</div>
+                        </div>
+                       
                 </div>
         );
 }
