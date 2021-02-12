af5b46bacfc335b7310930b9c0e72447cdcd3a81
Aksenov <ais924@yandex.ru>
2021-02-12 15:58:50 +0300

    Урок 29 state.js переносим данные из index.js

---
+++ src/components/Redux/state.js
@@ -0,0 +1,27 @@
+let state ={
+        profilePage:{
+                postData: [
+                        {id:1, post:'Это мой первый пост', like: 5},
+                        {id:2, post:'Привет, как дела?', like: 2},
+                        {id:3, post:'Хуёво, женег нет', like: 10},
+                      ],
+        },
+        dialogsPage:{
+                dialogsData: [
+                        {id: 1, name: 'Ilya'},
+                        {id: 2, name: 'Anna'},
+                        {id: 3, name: 'Aleksey'},
+                        {id: 4, name: 'Egor'},
+                        {id: 5, name: 'Konstantin'},
+                ],
+                      
+                messagesData: [
+                        {id: 1, message: 'Привет'},
+                        {id: 2, message: 'Как дела'},
+                        {id: 3, message: 'Учу ссучий реакт'},
+                        {id: 4, message: 'да, пердак то горит'}
+                ],
+        }
+}
+
+export default state;
--- src/index.js
+++ src/index.js
@@ -3,32 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-let postData =[
-  {id:1, post:'Это мой первый пост', like: 5},
-  {id:2, post:'Привет, как дела?', like: 2},
-  {id:3, post:'Хуёво, женег нет', like: 10},
-]
-
-let dialogsData = [
-  {id: 1, name: 'Ilya'},
-  {id: 2, name: 'Anna'},
-  {id: 3, name: 'Aleksey'},
-  {id: 4, name: 'Egor'},
-  {id: 5, name: 'Konstantin'},
-]
-
-let messagesData = [
-  {id: 1, message: 'Привет'},
-  {id: 2, message: 'Как дела'},
-  {id: 3, message: 'Учу ссучий реакт'},
-  {id: 4, message: 'да, пердак то горит'}
-]
+import state from './components/Redux/state'
 
 ReactDOM.render(
   
   <React.StrictMode>
-    <App postData={postData} dialogsData={dialogsData} messagesData={messagesData} />
+    <App 
+      postData={state.profilePage.postData} 
+      dialogsData={state.dialogsPage.dialogsData} 
+      messagesData={state.dialogsPage.messagesData} />
   </React.StrictMode>,
   document.getElementById('root')
 );
