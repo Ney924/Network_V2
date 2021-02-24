a72e49fb77bd3421c803fc25e26ea5db8598cee3
Aksenov <ais924@yandex.ru>
2021-02-24 16:19:02 +0300

    Урок 51 подтягиваем Users с сервера. get запрос

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,63 +1,15 @@
 import React from 'react';
 import './Users.css';
+import * as axios from 'axios';
+import noAvatar from './../assets/images/no_avatar.png';
 
 
 let Users = (props) => {
         if (props.usersData.length === 0)
                 {
-                        props.setUsers([
-                        {       id: 1, 
-                                fullName: 'Ilya', 
-                                avatarUsers: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2FEQjYeVYv86TI-kFJ0T4mu52NIKwfTz50Q&usqp=CAU', 
-                                follow: true, 
-                                statusUsers: 'Зачем я ваще сюда полез',
-                                location: {
-                                        city: 'Moscau',
-                                        country: 'Russian'
-                                },
-                        },
-                        {       id: 2, 
-                                fullName: 'Anna', 
-                                avatarUsers: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2FEQjYeVYv86TI-kFJ0T4mu52NIKwfTz50Q&usqp=CAU', 
-                                follow: true, 
-                                statusUsers: 'Чтобы мы с тобой жили как люди',
-                                location: {
-                                        city: 'Moscau',
-                                        country: 'Russian'
-                                },
-                        },
-                        {       id: 3, 
-                                fullName: 'Aleksey', 
-                                avatarUsers: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2FEQjYeVYv86TI-kFJ0T4mu52NIKwfTz50Q&usqp=CAU', 
-                                follow: true, 
-                                statusUsers: 'Хватит ныть и занимайся',
-                                location: {
-                                        city: 'Moscau',
-                                        country: 'Russian'
-                                },
-                        },
-                        {       id: 4, 
-                                fullName: 'Egor', 
-                                avatarUsers: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2FEQjYeVYv86TI-kFJ0T4mu52NIKwfTz50Q&usqp=CAU', 
-                                follow: true, 
-                                statusUsers: 'Пойду пивка дам',
-                                location: {
-                                        city: 'Moscau',
-                                        country: 'Russian'
-                                },
-                        },
-                        {       id: 5, 
-                                fullName: 'Konstantin', 
-                                avatarUsers: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2FEQjYeVYv86TI-kFJ0T4mu52NIKwfTz50Q&usqp=CAU', 
-                                follow: true, 
-                                statusUsers: 'Ремонт делать не лучше',
-                                location: {
-                                        city: 'Moscau',
-                                        country: 'Russian'
-                                },
-                        },
-                ],
-                ) 
+                        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+                                props.setUsers(response.data.items);   
+                        })       
         }           
         
         return (
@@ -65,7 +17,7 @@ let Users = (props) => {
                         {props.usersData.map(u => 
                                         <div className='users' key={u.id}>
                                                 <div className='users-avatar'>
-                                                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS93hT0wZKzLDrIruoP03lcSVjIm1ilEp9VeQ&usqp=CAU"/>
+                                                        <div>{u.photos.small !=null ? u.photos.small : noAvatar}</div>
                                                         <div>
                                                                 {u.follow 
                                                                         ? <button onClick={ ()=> {props.unFollow(u.id)}}>Подписаться</button>
@@ -75,12 +27,12 @@ let Users = (props) => {
                                                         </div>
                                                 </div>
                                                 <div className='users-name'>
-                                                        <div>{u.fullName}</div>
+                                                        <div>{u.name}</div>
                                                         <div className='status'>{u.status}</div>
                                                 </div>
                                                 <div className='users-locaton'>
-                                                        <div>{u.location.country}</div>
-                                                        <div>{u.location.city}</div>
+                                                        <div>{'u.location.country'}</div>
+                                                        <div>{'u.location.city'}</div>
                                                 </div>
                                         </div>
                                 )}
