fd6ec16b359af5dd944c89d93585192f73df349f
Aksenov <ais924@yandex.ru>
2021-02-28 11:32:45 +0300

    Урок 62 практика, follow-unfollow api

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Users.css';
 import noAvatar from '../image/no_avatar.png';
 import {NavLink} from 'react-router-dom'
+import * as axios from 'axios';
 
 let Users = (props) => {
         let pagesCount = Math.ceil(props.totalCount/props.pagesSize)
@@ -34,8 +35,22 @@ let Users = (props) => {
                                                 </NavLink>
                                                 <div className='users-follow-button'>
                                                         {u.follow 
-                                                                ? <button onClick={ ()=> {props.unFollow(u.id)}}>Подписаться</button>
-                                                                : <button onClick={ ()=> {props.follow(u.id)}}>Отписаться</button>
+                                                                ? <button onClick={ ()=> {
+                                                                        axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {withCredentials: true, headers: {"API-KEY": "785eab70-df14-4fb5-9f9e-100f69f4a368"}})
+                                                                        .then( response => {
+                                                                                if (response.data.resultCode==0) {
+                                                                                        props.unFollow(u.id)  
+                                                                                }
+                                                                        });    
+                                                                }}>Отписаться</button>
+                                                                : <button onClick={ ()=> {
+                                                                        axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {withCredentials: true, headers: {'API-KEY': '785eab70-df14-4fb5-9f9e-100f69f4a368'}})
+                                                                        .then( response => {
+                                                                                if (response.data.resultCode==0) {
+                                                                                        props.follow(u.id)  
+                                                                                }
+                                                                        });
+                                                                }}>Подписаться</button>
                                                         }
                                                 </div>
                                         </div>
