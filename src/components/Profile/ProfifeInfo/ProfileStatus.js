d4c7494841a717ffaf32aa7c7d97d661bd874e2e
Aksenov <ais924@yandex.ru>
2021-03-06 13:33:09 +0300

    Урок 74 componentDidUpdate

--- src/components/Profile/ProfifeInfo/ProfileStatus.js
+++ src/components/Profile/ProfifeInfo/ProfileStatus.js
@@ -13,8 +13,8 @@ class ProfileStatus extends React.Component {
         }
         
         deactivateEditMode = () => {
-                this.setState({editMode: false})
                 this.props.updateUserStatusTC(this.state.status)
+                this.setState({editMode: false}) 
         }
 
         onStatusChange = (e) => {
@@ -23,17 +23,32 @@ class ProfileStatus extends React.Component {
                 })
         }
 
+        componentDidUpdate(prevProps, prevState) {
+                if (prevProps.status !== this.props.status) {
+                        this.setState({
+                                status: this.props.status
+                        })
+                }
+        }
+
         render () { 
                return (
                         <div className='profile-status'>
                         {this.state.editMode===false
-                                ?<div onDoubleClick={this.activateEditMode}>{this.props.aboutMe||'No status'} </div>
-                                :<input 
-                                        onChange={this.onStatusChange}
-                                        value={this.props.aboutMe} 
-                                        onBlur={this.deactivateEditMode} 
-                                        autoFocus={true}
-                                />} 
+                                ?<div className='profile-status-now'
+                                         onDoubleClick={this.activateEditMode}>
+                                        {this.props.aboutMe||'No status'} 
+                                </div>
+                                :<div className='profile-status-change'>
+                                        <input 
+                                                onChange={this.onStatusChange}
+                                                value={this.props.aboutMe} 
+                                                onBlur={this.deactivateEditMode} 
+                                                autoFocus={true}
+                                        />
+                                        <button>Сохранить</button>
+                                </div>
+                                } 
                         
         </div>      
        )
